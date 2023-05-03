4d5cf0c35f0533977a7b7f233a6c59207f2819a4
hamzaali81 <hamza54834@gmail.com>
2023-05-03 16:30:11 -0400

    chapter03-complete

--- JS-Book-ELoquent-JS/lecture03.js
+++ JS-Book-ELoquent-JS/lecture03.js
@@ -3,14 +3,14 @@
 // a function.
 
 // Example: (Exponential)
-const power = function(base, exponent) {
-    let result = 1;
-    for (let count = 0; count < exponent; count++) {
-    result *= base;
-    }
-    return result;
-    };
-console.log(power(2, 3));
+// const power = function(base, exponent) {
+//     let result = 1;
+//     for (let count = 0; count < exponent; count++) {
+//     result *= base;
+//     }
+//     return result;
+//     };
+// console.log(power(2, 3));
 
 // Bindings declared with let and const are in fact local to the block that they
 // are declared in, so if you create one of those inside of a loop, the code before and
@@ -29,18 +29,18 @@ console.log(x + z);
 
 // Function As Values
 
-let launchMissiles = function() {
-    missileSystem.launch("now");
-    };
-    if (safeMode) {
-    launchMissiles = function() {/* do nothing */};
-    }
+// let launchMissiles = function() {
+//     missileSystem.launch("now");
+//     };
+//     if (safeMode) {
+//     launchMissiles = function() {/* do nothing */};
+//     }
 
 // Function Decleration
 
-function future() {
-    return "You'll never have flying cars";
-    }
+// function future() {
+//     return "You'll never have flying cars";
+//     }
 
 // Arrow Function
 // const power = (base, exponent) => {
@@ -53,8 +53,8 @@ function future() {
 
 // Optional Arguments
 
-function square(x) { return x * x; }
-console.log(square(4, true, "hedgehog"));
+// function square(x) { return x * x; }
+// console.log(square(4, true, "hedgehog"));
 // → 16
 
 // Closure
@@ -86,8 +86,7 @@ return base * power(base, exponent - 1);
 }
 }
 
-
-
+console.log(power(2, 3));
 // Recursion is not always just an ineﬀicient alternative to looping. Some prob-
 // lems really are easier to solve with recursion than with loops. Most often these
 // are problems that require exploring or processing several “branches”, each of
@@ -99,20 +98,20 @@ return base * power(base, exponent - 1);
 // For example, the number 13 could be reached by first multiplying by 3 and
 // then adding 5 twice, whereas the number 15 cannot be reached at all.
 // Here is a recursive solution:
-function findSolution(target) {
-function find(current, history) {
-if (current == target) {
-return history;
-} else if (current > target) {
-return null;
-} else {
-return find(current + 5, `(${history} + 5)`) ||
-find(current * 3, `(${history} * 3)`);
-}
-}
-return find(1, "1");
-}
-console.log(findSolution(24));
+// function findSolution(target) {
+// function find(current, history) {
+// if (current == target) {
+// return history;
+// } else if (current > target) {
+// return null;
+// } else {
+// return find(current + 5, `(${history} + 5)`) ||
+// find(current * 3, `(${history} * 3)`);
+// }
+// }
+// return find(1, "1");
+// }
+// console.log(findSolution(24));
 // → (((1 * 3) + 5) * 3)
 
 
@@ -136,14 +135,7 @@ console.log(findSolution(24));
 // function that calls itself is called recursive. Recursion allows some functions to
 // be written in a diﬀerent style. Take, for example, this alternative implementation
 // of power:
-function power(base, exponent) {
-if (exponent == 0) {
-return 1;
-}
-else {
-return base * power(base, exponent - 1);
-}
-}
+
 // This is rather close to the way mathematicians define exponentiation and
 
 // Function and Side Effects
